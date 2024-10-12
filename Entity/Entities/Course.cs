ea26e53d7634174f9ddb51c3034f41d01fe3c68e
Nazmiyee1102 <159723558+Nazmiyee1102@users.noreply.github.com>
2024-10-12 23:49:04 +0300

    Add project files.

---
+++ Entity/Entities/Course.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlamaio.Entity.Entities
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+
+        public string CourseImage { get; set; }
+
+        public string CourseName { get; set; }
+
+        public string Description { get; set; }
+
+        public int? InstructorId { get; set; }
+
+        public string InstructorName { get; set; }
+
+        public string InstructorImage { get; set; }
+
+        public Instructor Instructor { get; set; }
+
+        public List<Instructor> Instructors { get; set; }
+
+
+    }
+}
---
+++ Entity/Entities/Instructor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlamaio.Entity.Entities
+{
+    public class Instructor
+    {
+        public int InstructorId { get; set; }
+
+        public string InstructorName { get; set; }
+
+        public string Email { get; set; }
+
+        public string InstructorImage { get; set; }
+
+
+    }
+}
